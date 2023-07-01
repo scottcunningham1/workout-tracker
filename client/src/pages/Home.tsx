6f3b0a3edbe7b107633581688523ba809d741205
Scott Cunningham <hello@scottcunningham.net>
2023-07-01 17:44:15 +0100

    Basic routing setup + deliberate test fail

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return (
+        <h1 className="text-xl font-semibold">Home page</h1>
+    );
+};
+
+export default HomePage;
---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+const NotFoundPage = () => {
+    return (
+        <h1 className="text-xl font-semibold">404 Page not found</h1>
+    );
+};
+
+export default NotFoundPage;
